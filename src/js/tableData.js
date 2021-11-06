3824ea494e5dbd7c0b6855362c853236106bc10b
Marcin Ostaszewski <m.ostaszewski@azagroup.eu>
2021-11-06 17:56:38 +0100

    add table data object

---
+++ src/js/tableData.js
@@ -0,0 +1,367 @@
+export const tableData = {
+    date: "THU OCT 01 10:20 PM",
+    rows: [
+        {
+            rowTitle: "Spread",
+            teams: {
+                over: {
+                    name: {
+                        long: "Denver Broncos",
+                        short: "DNV"
+                    },
+                    consensus: 2,
+                    bookmakers: [
+                        {
+                            title: "Draftings sportbook",
+                            logoUrl: "url...",
+                            line: 2,
+                            odds: 1.91,
+                            isMax: true
+                        },
+                        {
+                            title: "Fanduel sportbook",
+                            logoUrl: "url...",
+                            line: 1.5,
+                            odds: 1.91,
+                            isMax: false
+                        },
+                        {
+                            title: "PointsBet",
+                            logoUrl: "url...",
+                            line: 1.5,
+                            odds: 1.91,
+                            isMax: false
+                        },
+                        {
+                            title: "BetRivers sportbook",
+                            logoUrl: "url...",
+                            line: 2,
+                            odds: 1.91,
+                            isMax: true
+                        },
+                        {
+                            title: "Sugar house",
+                            logoUrl: "url...",
+                            line: 2,
+                            odds: 1.91,
+                            isMax: true
+                        },
+                        {
+                            title: "Utest booker",
+                            logoUrl: "url...",
+                            line: 1.5,
+                            odds: 1.91,
+                            isMax: false
+                        },
+                        {
+                            title: "Tester betclicker",
+                            logoUrl: "url...",
+                            line: 1.5,
+                            odds: 1.91,
+                            isMax: false
+                        }
+                    ]
+                },
+                under: {
+                    name: {
+                        long: "New York Jets",
+                        short: "NYJ"
+                    },
+                    consensus: 2,
+                    bookmakers: [
+                        {
+                            title: "Draftings sportbook",
+                            logoUrl: "url...",
+                            line: -2,
+                            odds: 1.91,
+                            isMax: true
+                        },
+                        {
+                            title: "Fanduel sportbook",
+                            logoUrl: "url...",
+                            line: -1.5,
+                            odds: 1.91,
+                            isMax: false
+                        },
+                        {
+                            title: "PointsBet",
+                            logoUrl: "url...",
+                            line: -1.5,
+                            odds: 1.91,
+                            isMax: false
+                        },
+                        {
+                            title: "BetRivers sportbook",
+                            logoUrl: "url...",
+                            line: -2,
+                            odds: 1.91,
+                            isMax: true
+                        },
+                        {
+                            title: "Sugar house",
+                            logoUrl: "url...",
+                            line: -2,
+                            odds: 1.91,
+                            isMax: true
+                        },
+                        {
+                            title: "Utest booker",
+                            logoUrl: "url...",
+                            line: 1.5,
+                            odds: 1.91,
+                            isMax: false
+                        },
+                        {
+                            title: "Tester betclicker",
+                            logoUrl: "url...",
+                            line: 1.5,
+                            odds: 1.91,
+                            isMax: false
+                        }
+                    ]
+                }
+            }
+        },
+
+
+
+        {
+            rowTitle: "Totals",
+            teams: {
+                over: {
+                    name: {
+                        long: "over",
+                        short: "ovr"
+                    },
+                    consensus: 41,
+                    bookmakers: [
+                        {
+                            title: "Draftings sportbook",
+                            logoUrl: "url...",
+                            line: 41,
+                            odds: 1.91,
+                            isMax: true
+                        },
+                        {
+                            title: "Fanduel sportbook",
+                            logoUrl: "url...",
+                            line: 40.5,
+                            odds: 1.87,
+                            isMax: false
+                        },
+                        {
+                            title: "PointsBet",
+                            logoUrl: "url...",
+                            line: 41,
+                            odds: 1.91,
+                            isMax: false
+                        },
+                        {
+                            title: "BetRivers sportbook",
+                            logoUrl: "url...",
+                            line: 41,
+                            odds: 1.91,
+                            isMax: true
+                        },
+                        {
+                            title: "Sugar house",
+                            logoUrl: "url...",
+                            line: 41,
+                            odds: 1.91,
+                            isMax: true
+                        },
+                        {
+                            title: "Utest booker",
+                            logoUrl: "url...",
+                            line: 41,
+                            odds: 1.91,
+                            isMax: false
+                        },
+                        {
+                            title: "Tester betclicker",
+                            logoUrl: "url...",
+                            line: 40,
+                            odds: 1.71,
+                            isMax: false
+                        }
+                    ]
+                },
+                under: {
+                    name: {
+                        long: "under",
+                        short: "und"
+                    },
+                    consensus: 41,
+                    bookmakers: [
+                        {
+                            title: "Draftings sportbook",
+                            logoUrl: "url...",
+                            line: 41,
+                            odds: 1.91,
+                            isMax: true
+                        },
+                        {
+                            title: "Fanduel sportbook",
+                            logoUrl: "url...",
+                            line: 40.5,
+                            odds: 1.87,
+                            isMax: false
+                        },
+                        {
+                            title: "PointsBet",
+                            logoUrl: "url...",
+                            line: 41,
+                            odds: 1.91,
+                            isMax: false
+                        },
+                        {
+                            title: "BetRivers sportbook",
+                            logoUrl: "url...",
+                            line: 41,
+                            odds: 1.91,
+                            isMax: true
+                        },
+                        {
+                            title: "Sugar house",
+                            logoUrl: "url...",
+                            line: 41,
+                            odds: 1.91,
+                            isMax: true
+                        },
+                        {
+                            title: "Utest booker",
+                            logoUrl: "url...",
+                            line: 41,
+                            odds: 1.91,
+                            isMax: false
+                        },
+                        {
+                            title: "Tester betclicker",
+                            logoUrl: "url...",
+                            line: 40,
+                            odds: 1.71,
+                            isMax: false
+                        }
+                    ]
+                }
+            }
+        },
+
+
+
+        {
+            rowTitle: "Moneyline",
+            teams: {
+                over: {
+                    name: {
+                        long: "Denver Broncos",
+                        short: "DNV"
+                    },
+                    consensus: 2.1,
+                    bookmakers: [
+                        {
+                            title: "Draftings sportbook",
+                            logoUrl: "url...",
+                            odds: 2.1,
+                            isMax: true
+                        },
+                        {
+                            title: "Fanduel sportbook",
+                            logoUrl: "url...",
+                            odds: 2.02,
+                            isMax: false
+                        },
+                        {
+                            title: "PointsBet",
+                            logoUrl: "url...",
+                            odds: 2.05,
+                            isMax: false
+                        },
+                        {
+                            title: "BetRivers sportbook",
+                            logoUrl: "url...",
+                            odds: 2.1,
+                            isMax: true
+                        },
+                        {
+                            title: "Sugar house",
+                            logoUrl: "url...",
+                            odds: 2.1,
+                            isMax: true
+                        },
+                        {
+                            title: "Utest booker",
+                            logoUrl: "url...",
+                            odds: 1.95,
+                            isMax: false
+                        },
+                        {
+                            title: "Tester betclicker",
+                            logoUrl: "url...",
+                            odds: 1.9,
+                            isMax: false
+                        }
+                    ]
+                },
+                under: {
+                    name: {
+                        long: "New York Jets",
+                        short: "NYJ"
+                    },
+                    consensus: 2,
+                    bookmakers: [
+                        {
+                            title: "Draftings sportbook",
+                            logoUrl: "url...",
+                            line: -2,
+                            odds: 1.91,
+                            isMax: true
+                        },
+                        {
+                            title: "Fanduel sportbook",
+                            logoUrl: "url...",
+                            line: -1.5,
+                            odds: 1.91,
+                            isMax: false
+                        },
+                        {
+                            title: "PointsBet",
+                            logoUrl: "url...",
+                            line: -1.5,
+                            odds: 1.91,
+                            isMax: false
+                        },
+                        {
+                            title: "BetRivers sportbook",
+                            logoUrl: "url...",
+                            line: -2,
+                            odds: 1.91,
+                            isMax: true
+                        },
+                        {
+                            title: "Sugar house",
+                            logoUrl: "url...",
+                            line: -2,
+                            odds: 1.91,
+                            isMax: true
+                        },
+                        {
+                            title: "Utest booker",
+                            logoUrl: "url...",
+                            line: 1.5,
+                            odds: 1.91,
+                            isMax: false
+                        },
+                        {
+                            title: "Tester betclicker",
+                            logoUrl: "url...",
+                            line: 1.5,
+                            odds: 1.91,
+                            isMax: false
+                        }
+                    ]
+                }
+            }
+        }
+    ]
+}
