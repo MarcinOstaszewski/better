d38255c7ff4a17a0cd10b3e72bd08cac1ac4e7e1
Marcin Ostaszewski <m.ostaszewski@azagroup.eu>
2021-11-07 17:24:55 +0100

    extend bookmakers data

--- src/js/tableData.js
+++ src/js/tableData.js
@@ -1,4 +1,4 @@
-export const tableData = {
+const tableData = {
     date: "THU OCT 01 10:20 PM",
     rows: [
         {
@@ -36,7 +36,98 @@ export const tableData = {
                 },
                 {
                     title: "Fanduel sportbook",
-                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Esteghlaldostaneh.jpg/320px-Esteghlaldostaneh.jpg",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Lombardia_Passo_dello_Stelvio_Ovest_21.jpg/320px-Lombardia_Passo_dello_Stelvio_Ovest_21.jpg",
+                    over: {
+                        line: 1.5,
+                        odds: 1.91,
+                    },
+                    under: {
+                        line: -1.5,
+                        odds: 1.91,
+                    },
+                    isMax: false
+                },
+                {
+                    title: "PointsBet",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Jungle_Cruise_Visual_Effects_Comparison.jpg/320px-Jungle_Cruise_Visual_Effects_Comparison.jpg",
+                    over: {
+                        line: 1.5,
+                        odds: 1.91,
+                    },
+                    under: {
+                        line: -1.5,
+                        odds: 1.91,
+                    },
+                    isMax: false
+                },
+                {
+                    title: "BetRivers sportbook",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Iba_27.jpg",
+                    over: {
+                        line: 2,
+                        odds: 1.91,
+                    },
+                    under: {
+                        line: -2,
+                        odds: 1.91,
+                    },
+                    isMax: true
+                },
+                {
+                    title: "Sugar house",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Logo_Oficial_de_la_Copa_Sim%C3%B3n_Bol%C3%ADvar_2021.png",
+                    over: {
+                        line: 2,
+                        odds: 1.91,
+                    },
+                    under: {
+                        line: -2,
+                        odds: 1.91,
+                    },
+                    isMax: true
+                },
+                {
+                    title: "Some test booker",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Logo-alsace-nature.png",
+                    over: {
+                        line: 1.5,
+                        odds: 1.91,
+                    },
+                    under: {
+                        line: -1.5,
+                        odds: 1.91,
+                    },
+                    isMax: false
+                },
+                {
+                    title: "Other booker",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Ysk_comic.jpg/320px-Ysk_comic.jpg",
+                    over: {
+                        line: 1.5,
+                        odds: 1.91,
+                    },
+                    under: {
+                        line: -1.5,
+                        odds: 1.91,
+                    },
+                    isMax: false
+                },
+                {
+                    title: "Draftings sportbook",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Logo_ch%C3%ADnh_c%E1%BB%A7a_phim.jpg/320px-Logo_ch%C3%ADnh_c%E1%BB%A7a_phim.jpg",
+                    over: {
+                        line: 2,
+                        odds: 1.91,
+                    },
+                    under: {
+                        line: -2,
+                        odds: 1.91,
+                    },
+                    isMax: true
+                },
+                {
+                    title: "Fanduel sportbook",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Lombardia_Passo_dello_Stelvio_Ovest_21.jpg/320px-Lombardia_Passo_dello_Stelvio_Ovest_21.jpg",
                     over: {
                         line: 1.5,
                         odds: 1.91,
@@ -148,7 +239,98 @@ export const tableData = {
                 },
                 {
                     title: "Fanduel sportbook",
-                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Esteghlaldostaneh.jpg/320px-Esteghlaldostaneh.jpg",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Lombardia_Passo_dello_Stelvio_Ovest_21.jpg/320px-Lombardia_Passo_dello_Stelvio_Ovest_21.jpg",
+                    over: {
+                        line: 40.5,
+                        odds: 1.87,
+                    },
+                    under: {
+                        line: 40.5,
+                        odds: 1.95,
+                    },
+                    isMax: false
+                },
+                {
+                    title: "PointsBet",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Jungle_Cruise_Visual_Effects_Comparison.jpg/320px-Jungle_Cruise_Visual_Effects_Comparison.jpg",
+                    over: {
+                        line: 41,
+                        odds: 1.91,
+                    },
+                    under: {
+                        line: 41,
+                        odds: 1.91,
+                    },
+                    isMax: false
+                },
+                {
+                    title: "BetRivers sportbook",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Iba_27.jpg",
+                    over: {
+                        line: 41,
+                        odds: 1.91,
+                    },
+                    under: {
+                        line: 41,
+                        odds: 1.91,
+                    },
+                    isMax: true
+                },
+                {
+                    title: "Sugar house",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Logo_Oficial_de_la_Copa_Sim%C3%B3n_Bol%C3%ADvar_2021.png",
+                    over: {
+                        line: 41,
+                        odds: 1.91,
+                    },
+                    under: {
+                        line: 41,
+                        odds: 1.91,
+                    },
+                    isMax: true
+                },
+                {
+                    title: "Utest booker",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Logo-alsace-nature.png",
+                    over: {
+                        line: 41,
+                        odds: 1.91,
+                    },
+                    under: {
+                        line: 41,
+                        odds: 1.91,
+                    },
+                    isMax: false
+                },
+                {
+                    title: "Tester betclicker",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Ysk_comic.jpg/320px-Ysk_comic.jpg",
+                    over: {
+                        line: 40,
+                        odds: 1.71,
+                    },
+                    under: {
+                        line: 40,
+                        odds: 1.71,
+                    },
+                    isMax: false
+                },
+                {
+                    title: "Draftings sportbook",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Logo_ch%C3%ADnh_c%E1%BB%A7a_phim.jpg/320px-Logo_ch%C3%ADnh_c%E1%BB%A7a_phim.jpg",
+                    over: {
+                        line: 41,
+                        odds: 1.91,
+                    },
+                    under: {
+                        line: 41,
+                        odds: 1.91,
+                    },
+                    isMax: true
+                },
+                {
+                    title: "Fanduel sportbook",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Lombardia_Passo_dello_Stelvio_Ovest_21.jpg/320px-Lombardia_Passo_dello_Stelvio_Ovest_21.jpg",
                     over: {
                         line: 40.5,
                         odds: 1.87,
@@ -261,7 +443,84 @@ export const tableData = {
                 },
                 {
                     title: "Fanduel sportbook",
-                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Esteghlaldostaneh.jpg/320px-Esteghlaldostaneh.jpg",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Lombardia_Passo_dello_Stelvio_Ovest_21.jpg/320px-Lombardia_Passo_dello_Stelvio_Ovest_21.jpg",
+                    over: {
+                        odds:  2.02,
+                    },
+                    under: {
+                        odds: 1.83,
+                    },
+                    isMax: false
+                },
+                {
+                    title: "PointsBet",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Jungle_Cruise_Visual_Effects_Comparison.jpg/320px-Jungle_Cruise_Visual_Effects_Comparison.jpg",
+                    over: {
+                        odds:  2.05,
+                    },
+                    under: {
+                        odds: 1.8,
+                    },
+                    isMax: false
+                },
+                {
+                    title: "BetRivers sportbook",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Iba_27.jpg",
+                    over: {
+                        odds:  2.1,
+                    },
+                    under: {
+                        odds: 1.8,
+                    },
+                    isMax: true
+                },
+                {
+                    title: "Sugar house",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Logo_Oficial_de_la_Copa_Sim%C3%B3n_Bol%C3%ADvar_2021.png",
+                    over: {
+                        odds:  2.02,
+                    },
+                    under: {
+                        odds: 1.83,
+                    },
+                    isMax: true
+                },
+                {
+                    title: "Utest booker",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Logo-alsace-nature.png",
+                    over: {
+                        odds:  1.9,
+                    },
+                    under: {
+                        odds: 1.63,
+                    },
+                    isMax: false
+                },
+                {
+                    title: "Tester betclicker",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Ysk_comic.jpg/320px-Ysk_comic.jpg",
+                    over: {
+                        odds:  1.95,
+                    },
+                    under: {
+                        odds: 1.6,
+                    },
+                    isMax: false
+                },
+                {
+                    title: "Draftings sportbook",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Logo_ch%C3%ADnh_c%E1%BB%A7a_phim.jpg/320px-Logo_ch%C3%ADnh_c%E1%BB%A7a_phim.jpg",
+                    over: {
+                        odds:  2.1,
+                    },
+                    under: {
+                        odds: 1.8,
+                    },
+                    isMax: true
+                },
+                {
+                    title: "Fanduel sportbook",
+                    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Lombardia_Passo_dello_Stelvio_Ovest_21.jpg/320px-Lombardia_Passo_dello_Stelvio_Ovest_21.jpg",
                     over: {
                         odds:  2.02,
                     },
